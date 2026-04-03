2ac548924fcea7deb0a5571c7805b508cf67d8f8
rethabilemanyanye4-coder <rethabilemanyanye4@gmail.com>
2026-04-03 17:03:05 +0200

    added the timezone to my javascript

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ function showSelectedCountry(event){
     if (event.target.length >0) {
         
         if (event.target.value === "Paris") {
-            alert()
+            let paristime= moment().tz("Europe/Paris").format("dddd, MMMM D, YYYY h:m A");
+            alert(`It is ${paristime} in Europe/Paris` );
             
         }
         if (event.target.value ==="Tokyo") {
-            alert()
+            let tokyoTime= moment().tz("Asia/Tokyo").format("dddd, MMMM D, YYYY h:m A");
+        
+        
+            alert(` It is ${tokyoTime} in Asia/Tokyo`) ;
+            
+                
+            
         }
         if (event.target.value ==="Sydney") {
-            alert()
+            let sydneyTime= moment().tz("Australia/Sydney").format("dddd, MMMM D, YYYY h:m A");
+            alert(`It is ${sydneyTime} in Australia/Sydney`);
         }
     }
+    
+    
 }
+
 let countriesSelected= document.querySelector("#countries");
 countriesSelected.addEventListener("change", showSelectedCountry);
